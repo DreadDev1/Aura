e527340b017b68860301c6470e5e876ab3769681
DreadDev1 <kameen.avery@gmail.com>
2023-08-31 12:36:31 -0400

    Section 3 - 2 - Health and Mana - UE5 GAS - Top Down RPG

--- Source/Aura/Private/Framework/AbilitySystem/Attributes/BaseAttributeSet.cpp
+++ Source/Aura/Private/Framework/AbilitySystem/Attributes/BaseAttributeSet.cpp
@@ -2,4 +2,48 @@
 
 
 #include "Framework/AbilitySystem/Attributes/BaseAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UBaseAttributeSet::UBaseAttributeSet()
+{
+}
+
+void UBaseAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+#pragma region	 Vital Attributes
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+#pragma endregion
+
+#pragma region Secondary Attributes
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);	
+#pragma endregion
+}
+
+#pragma region Vital Attributes
+void UBaseAttributeSet::OnRep_Health(const FGameplayAttributeData OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, Health, OldHealth);
+}
+
+void UBaseAttributeSet::OnRep_Mana(const FGameplayAttributeData OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, Mana, OldMana);
+}
+#pragma endregion
+
+#pragma region Secondary Attributes
+void UBaseAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UBaseAttributeSet::OnRep_MaxMana(const FGameplayAttributeData OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, MaxMana, OldMaxMana);
+}
+#pragma endregion 
--- Source/Aura/Public/Framework/AbilitySystem/Attributes/BaseAttributeSet.h
+++ Source/Aura/Public/Framework/AbilitySystem/Attributes/BaseAttributeSet.h
@@ -4,14 +4,51 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "BaseAttributeSet.generated.h"
 
-/**
- * 
- */
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+		GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+		GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+		GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+		GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+
 UCLASS()
 class AURA_API UBaseAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+	UBaseAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+public:
 	
+#pragma region Vital Attributes
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, Health)
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData OldHealth) const;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, Mana)
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData OldMana) const;
+#pragma endregion
+
+#pragma region Secondary Attributes
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Secondary Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, MaxHealth)
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData OldMaxHealth) const;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Secondary Attributes")
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, MaxMana)
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData OldMaxMana) const;
+#pragma endregion
 };
