5fb5992be8576717139fadb6c406dfbe8c8cf424
DreadDev1 <kameen.avery@gmail.com>
2023-08-30 22:38:32 -0400

    Section 1 - 9 - Movement Input - GAS - Top Down RPG

---
+++ Source/Aura/Public/Player/BasePlayerController.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BasePlayerController.generated.h"
+
+class UBaseInputConfig;
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ABasePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	ABasePlayerController();
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void SetupInputComponent() override;
+	
+private:
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+	// UPROPERTY(EditDefaultsOnly, Category = "Input")
+	// TObjectPtr<UBaseInputConfig> InputConfig;
+
+	void Move(const FInputActionValue& InputActionValue);
+
+};
