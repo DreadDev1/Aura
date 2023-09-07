38dcbc15619ed075474a989225192fc2eaafe270
DreadDev1 <kameen.avery@gmail.com>
2023-09-07 00:45:27 -0400

    Section 6 - 10 - Broadcasting Data Table Rows - UE5 GAS - Top Down RPG

--- Source/Aura/Public/UI/WidgetControllers/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetControllers/OverlayWidgetController.h
@@ -6,13 +6,6 @@
 #include "UI/WidgetControllers/BaseWidgetController.h"
 #include "OverlayWidgetController.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealtChangedSignature, float, NewHealth);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealtChangedSignature, float, NewMaxHealth);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
-
-class UBaseUserWidget;
-
 USTRUCT(BlueprintType)
 struct FUIWidgetRow : public FTableRowBase
 {
@@ -25,10 +18,20 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FText Message = FText();
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	TSubclassOf<UBaseUserWidget> MessageWidget;
+	TSubclassOf<class UBaseUserWidget> MessageWidget;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UTexture2D* Image = nullptr;	
 };
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealtChangedSignature, float, NewHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealtChangedSignature, float, NewMaxHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMessageWidgetRowSignature, FUIWidgetRow, Row);
+
+
+
+
 
 UCLASS()
 class AURA_API UOverlayWidgetController : public UBaseWidgetController
@@ -48,6 +51,8 @@ public:
 	FOnManaChangedSignature OnManaChanged;
 	UPROPERTY(BlueprintAssignable, Category="GAS|Attributes")
 	FOnMaxManaChangedSignature OnMaxManaChanged;
+	UPROPERTY(BlueprintAssignable, Category="GAS|Messages")
+	FMessageWidgetRowSignature MessageWidgetRowDelegate;
 
 protected:
 
