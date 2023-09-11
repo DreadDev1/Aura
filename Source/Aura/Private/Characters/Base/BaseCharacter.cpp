f0bf3258766c7921a66c4c45ed6e92ef2fcd1f5d
DreadDev1 <kameen.avery@gmail.com>
2023-09-10 22:44:16 -0400

    Section 7 - 10 - Modifier Magnitude Calculations - UE5 GAS - Top Down RPG

--- Source/Aura/Private/Characters/Base/BaseCharacter.cpp
+++ Source/Aura/Private/Characters/Base/BaseCharacter.cpp
@@ -32,10 +32,10 @@ void ABaseCharacter::ApplyEffectToSelf(TSubclassOf<UGameplayEffect> GameplayEffe
 {
 	check(IsValid(GetAbilitySystemComponent()));
 	check(GameplayEffectClass);
-	const FGameplayEffectContextHandle ContextHandle = GetAbilitySystemComponent()->MakeEffectContext();
+	FGameplayEffectContextHandle ContextHandle = GetAbilitySystemComponent()->MakeEffectContext();
+	ContextHandle.AddSourceObject(this);
 	const FGameplayEffectSpecHandle SpecHandle = GetAbilitySystemComponent()->MakeOutgoingSpec(GameplayEffectClass, Level, ContextHandle);
 	GetAbilitySystemComponent()->ApplyGameplayEffectSpecToTarget(*SpecHandle.Data.Get(), GetAbilitySystemComponent());
-
 }
 
 void ABaseCharacter::InitializeDefaultAttributes() const
