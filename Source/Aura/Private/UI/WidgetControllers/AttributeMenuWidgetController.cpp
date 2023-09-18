286792704c61402ffe0549ffbb3ffbc915cbbb83
DreadDev1 <kameen.avery@gmail.com>
2023-09-17 21:28:27 -0400

    Section 8 - 21 - Responding to Attribute Changes - UE5 GAS - Top Down RPG

--- Source/Aura/Private/UI/WidgetControllers/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetControllers/AttributeMenuWidgetController.cpp
@@ -9,7 +9,17 @@
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
-	
+	UBaseAttributeSet* AS = CastChecked<UBaseAttributeSet>(AttributeSet);
+	check(AttributeInfo);
+	for (auto& Pair : AS->TagsToAttributes)
+	{
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Pair.Value()).AddLambda(
+		[this, Pair](const FOnAttributeChangeData& Data)
+		{
+			BroadcastAttributeInfo(Pair.Key, Pair.Value());
+		}
+	);
+	}
 }
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
@@ -18,8 +28,14 @@ void UAttributeMenuWidgetController::BroadcastInitialValues()
 	check(AttributeInfo);
 	for (auto& Pair : AS->TagsToAttributes)
 	{
-		FBaseAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(Pair.Key);
-		Info.AttributeValue = Pair.Value().GetNumericValue(AS);
-		AttributeInfoDelegate.Broadcast(Info);
+		BroadcastAttributeInfo(Pair.Key, Pair.Value());
 	}
+}
+
+void UAttributeMenuWidgetController::BroadcastAttributeInfo(const FGameplayTag& AttributeTag,
+	const FGameplayAttribute& Attribute) const
+{
+	FBaseAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(AttributeTag);
+	Info.AttributeValue = Attribute.GetNumericValue(AttributeSet);
+	AttributeInfoDelegate.Broadcast(Info);
 }
--- Source/Aura/Public/UI/WidgetControllers/AttributeMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetControllers/AttributeMenuWidgetController.h
@@ -29,4 +29,6 @@ protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	TObjectPtr<UAttributeInfo> AttributeInfo;
 
+private:
+	void BroadcastAttributeInfo(const FGameplayTag& AttributeTag, const FGameplayAttribute& Attribute) const;
 };
